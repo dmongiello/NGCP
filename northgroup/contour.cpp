c02571b8dfd3fef3605d1cc0a179168987c728bf
MicPrez <mic619er@gmail.com>
2016-01-30 12:40:05 -0800

    Update contour.cpp

--- northgroup/contour.cpp
+++ northgroup/contour.cpp
@@ -1,35 +1,14 @@
 #include <iostream>
 #include <stdio.h>
-//                         This is for windows to find includes ///////////////////
-//#include "stdafx.h"
-//#include <iostream>
-//#include <stdio.h>
-//#include <opencv2\opencv.hpp>
-//#include "opencv2\highgui\highgui.hpp"
-//#include "opencv2\imgproc\imgproc.hpp"
-//#include "opencv2\core\core.hpp"
-//#include "opencv2\features2d\features2d.hpp"
-//#include "opencv2\nonfree\nonfree.hpp"
-//#include "opencv2\calib3d\calib3d.hpp"
-//#include <opencv2\video\background_segm.hpp>
-
-
-// These are Mac includes comment these out and uncomment the windows includes////////
-
-#include <iostream>
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include < opencv2\opencv.hpp>  
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/features2d/features2d.hpp"
-#include "opencv2/features2d.hpp" // mac version of nonfree.hpp
-//#include "opencv2/nonfree/nonfree.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
-#include <opencv2/video/background_segm.hpp>
-
+#include < opencv2/video/background_segm.hpp> 
 #include <cmath>
-
 #include "isrheader.h"
 
 using namespace cv;
@@ -40,7 +19,7 @@ using namespace std;
 void contour(Mat& imgThresholded, Mat& imgOriginal, Mat& img)
 {
 	Mat bw;
-	Canny(imgThresholded, bw, 0, 50, 5);
+	Canny(imgThresholded, bw, 0, 50, 5); //coutour function
 	vector<std::vector<cv::Point> > contours;
 
 	findContours(bw, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
