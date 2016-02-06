bbe18ea32d58971bf25440ecd4e7d65f6f3dd01a
ktwinterer <kylewinterer@yahoo.com>
2016-02-06 11:11:50 -0800

    010616
    Commented code and removed unneeded

--- northgroup/contour.cpp
+++ northgroup/contour.cpp
@@ -40,34 +40,30 @@ using namespace std;
 void contour(Mat& imgThresholded, Mat& imgOriginal, Mat& img)
 {
 	Mat bw;
-	Canny(imgThresholded, bw, 0, 50, 5);
-	vector<std::vector<cv::Point> > contours;
+	Canny(imgThresholded, bw, 0, 50, 5); //Edge Detector 
+	vector<vector<Point>> contours;
 
 	findContours(bw, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
 	/// Approximate contours to polygons + get bounding rects and circles
-	vector<vector<Point> > contours_draw(contours.size());
-	vector<cv::Point> contours_poly;
+	vector<vector<Point>> contours_draw(contours.size());
+	vector<Point>contours_poly;
 
 	for (int i = 0; i < contours.size(); i++) //set labels or identify shapes in this loop
 	{
-		approxPolyDP(Mat(contours[i]), contours_poly, 1, true);
-		approxPolyDP(Mat(contours[i]), contours_draw[i], 1, true);
+		double area = contourArea(contours[i]);
+		Rect r = boundingRect(contours[i]);
+		int radius = r.width / 2;//simple way of finding radius. this should be updated to be more complex
 
-		double area = cv::contourArea(contours[i]);
-		cv::Rect r = cv::boundingRect(contours[i]);
-		int radius = r.width / 2;
-
-		if (abs(1 - ((double)r.width / r.height)) <= 0.2 && abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2 && area > 200)
-		{
-			setLabel(imgOriginal, "CIR", contours[i]);
+		if (abs - ((double)r.width / r.height)) <= 0.2 && abs(1 - (area / (CV_PI * std::pow(radius, 2)))) <= 0.2 && area > 200) //area the countour has to fit the theoretical area of a circle
+		{(
+			setLabel(imgOriginal, "CIR", contours[i]); //give stick cir on it to indentify it
 			Mat drawing = Mat::zeros(imgThresholded.size(), CV_8UC3);
 			for (int i = 0; i< contours.size(); i++)
 			{
 				Scalar color = Scalar(0, 0, 0);
-				drawContours(imgOriginal, contours_draw, i, color, 5, 8, vector<Vec4i>(), 0, Point());
+				drawContours(imgOriginal, contours_draw, i, color, 5, 8, vector<Vec4i>(), 0, Point());	//Draw around the found target
 				drawContours(img, contours_draw, i, color, 5, 8, vector<Vec4i>(), 0, Point());
-				//rectangle(imgOriginal, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
-				//circle(imgOriginal, center[i], (int)radius[i], color, 2, 8, 0);
+				
 			}
 		}
 	}
