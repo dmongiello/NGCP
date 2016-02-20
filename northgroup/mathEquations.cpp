4f9c6e36914ce75d78004759c8ff7e6644d0fb4b
MicPrez <mic619er@gmail.com>
2016-02-20 13:56:33 -0800

    Update mathEquations.cpp

--- northgroup/mathEquations.cpp
+++ northgroup/mathEquations.cpp
@@ -1,11 +1,30 @@
-//
-//  mathEquations.cpp
-//  northgroup
-//
-//  Created by David Mongiello on 2/14/16.
 //  Copyright © 2016 David Mongiello. All rights reserved.
 //
-
+#include <iomanip>
+#include <iostream>
+#include <sys/utime.h>
 #include "main.hpp"
+#include HoughCircle.cpp
+using namespace std;
 
-// The file is to hold all the math formula functions for geting angles and radiuses. 
+
+void angleRad(Mat imgThresholded){
+
+	double widX = imgThresholded.cols;//takes width of img
+	double lenY = imgThresholded.rows;//takes length of img
+	double x = widX / 2;
+	double y = lenY / 2;
+
+	double arrayCen[2] = { (x, y };//center of image
+	double diff[2] = { x - cenX, y - cenY };//find distance from center image to center circ(not sure what the var for cent
+											//is used a filler
+	
+
+	
+	double rad = sqrt(diff[0]*diff[0] + diff[1]*diff[1]);
+	double angle = atan(diff[0] / diff[1]);
+	cout << time(0) << endl;
+	
+
+
+} 
